ef4341813643ab8a74a59df6dca6d50661421970
mariom <mariom@backbase.com>
2022-10-17 14:18:03 -0500

    first configuration

---
+++ algoritmos.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+    
+func twoSum(nums: [Int],target: Int) -> [Int] {
+    var solutionArray : [Int] = []
+    let nums1 : Int = 0
+    while (nums1 < nums.count) {
+        print("hola")
+        if nums[nums1] + nums[] {
+            
+        }
+    }
+    return solutionArray
+}
+    
+
+var newArray = [1,2,3,4,5]
+var newTarget = 5
+twoSum(nums: newArray, target: newTarget)
