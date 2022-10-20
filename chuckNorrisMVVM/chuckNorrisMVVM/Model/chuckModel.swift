2ae4e0928e76638fdb084d9c5632c7b66aa3f6fe
mariom <mariom@backbase.com>
2022-10-20 14:44:08 -0500

    add localizable file

---
+++ chuckNorrisMVVM/chuckNorrisMVVM/Model/chuckModel.swift
@@ -0,0 +1,17 @@
+//
+//  model.swift
+//  chuckNorrisMVVM
+//
+//  Created by Mario Alberto Saldana Martinez on 18/10/22.
+//
+
+import Foundation
+
+// MARK: - Welcome8
+struct appModel {
+    let createdAt: String
+    let iconURL: String
+    let id, updatedAt: String
+    let url: String
+    let value: String
+}
